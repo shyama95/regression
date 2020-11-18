b8c783c43d5e7558a257ea8a0a12a9e61d682791
Shyama <shyamap95@gmail.com>
2020-11-18 23:59:24 +0530

    Initial commit

---
+++ regression.py
@@ -0,0 +1,51 @@
+import requests
+import pandas
+import scipy
+import numpy
+import sys
+
+
+TRAIN_DATA_URL = "https://storage.googleapis.com/kubric-hiring/linreg_train.csv"
+TEST_DATA_URL = "https://storage.googleapis.com/kubric-hiring/linreg_test.csv"
+
+
+def predict_price(area) -> float:
+    """
+    This method must accept as input an array `area` (represents a list of areas sizes in sq feet) and must return the respective predicted prices (price per sq foot) using the linear regression model that you build.
+
+    You can run this program from the command line using `python3 regression.py`.
+    """
+    response = requests.get(TRAIN_DATA_URL)
+    # YOUR IMPLEMENTATION HERE
+    data = response.text
+    areas = data.split('\n')[0]
+    areas = areas.split(',')[1:]
+    prices = data.split('\n')[0]
+    prices = prices.split(',')[1:]
+
+    areas = np.array([float(a) for a in areas])
+    prices = np.array([float(p) for p in prices])
+    
+    m = 0.0
+    c = 0.0
+
+    for i in range(10):
+        y_ = m*areas + c
+        e = numpy.mean((y_ - prices) ** 2)
+
+    return None
+
+
+if __name__ == "__main__":
+    # DO NOT CHANGE THE FOLLOWING CODE
+    from data import validation_data
+    areas = numpy.array(list(validation_data.keys()))
+    prices = numpy.array(list(validation_data.values()))
+    predicted_prices = predict_price(areas)
+    rmse = 0#numpy.sqrt(numpy.mean((predicted_prices - prices) ** 2))
+    try:
+        assert rmse < 170
+    except AssertionError:
+        print(f"Root mean squared error is too high - {rmse}. Expected it to be under 170")
+        sys.exit(1)
+    print(f"Success. RMSE = {rmse}")
